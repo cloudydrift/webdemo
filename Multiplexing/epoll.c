aee718a8978ed3980eb55ade4c241616ebdf8fc8
cloudydrift <cloudydrift@debian11.VirtualBox>
2022-06-29 22:49:36 -0400

    some fix

--- Multiplexing/epoll.c
+++ Multiplexing/epoll.c
@@ -2,23 +2,21 @@
 
 #define MAXEVENTS 10
 
-void echo(int fd);
-
 int main(int argc, char *argv[]) {
     struct sockaddr_storage clientaddr;
     struct epoll_event ev, events[MAXEVENTS];
     socklen_t clientlen = sizeof(clientaddr);
 	int listenfd, connfd, epollfd, nfds;
+    char buf[MAXLINE];
 	
 	if (argc != 2) {
 		fprintf(stderr, "usage %s <port>\n", argv[0]);
 		exit(0);
 	}
-	
-	listenfd = open_listenfd(argv[1]);
     
-    epollfd = epoll_create(10);
+    epollfd = epoll_create1(0);
     
+    listenfd = open_listenfd(argv[1]);
     ev.events = EPOLLIN;
     ev.data.fd = listenfd;
     epoll_ctl(epollfd, EPOLL_CTL_ADD, listenfd, &ev);
@@ -28,25 +26,20 @@ int main(int argc, char *argv[]) {
         for (int i = 0; i < nfds; i++) {
             if (events[i].data.fd == listenfd) {
                 connfd = accept(listenfd, (SA *)&clientaddr, &clientlen);
-                ev.events = EPOLLIN;
+                ev.events = EPOLLIN | EPOLLRDHUP;
                 ev.data.fd = connfd;
                 epoll_ctl(epollfd, EPOLL_CTL_ADD, connfd, &ev);
             } else {
-                echo(events[i].data.fd);
+                if (events[i].events & (EPOLLRDHUP | EPOLLERR)) {
+                    close(events[i].data.fd);
+                    epoll_ctl(epollfd, EPOLL_CTL_DEL, connfd, NULL);
+                } else {
+                    read(events[i].data.fd, buf, MAXLINE);
+                    printf("%s\n", buf);
+                }
             }
         }
 	}
 	return 0;
-}
-
-void echo(int connfd) {
-    size_t n;
-    char buf[MAXLINE];
-    rio_t rio;
-    rio_readinitb(&rio, connfd);
-    while((n = rio_readlineb(&rio, buf, MAXLINE)) != 0) {
-        printf("server received %d bytes\n", (int) n);
-        rio_writen(connfd, buf, n);
-    }
 }
 
