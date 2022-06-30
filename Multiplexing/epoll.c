5a7bbee712201a8600fbd7d9b2a0d01e657f0d54
cloudydrift <cloudydrift@debian11.VirtualBox>
2022-06-30 02:33:54 -0400

    some fix

--- Multiplexing/epoll.c
+++ Multiplexing/epoll.c
@@ -36,20 +36,24 @@ int main(int argc, char *argv[]) {
             if (fd1 == listenfd) {
                 clientlen = sizeof(clientaddr);
                 connfd = accept(listenfd, (SA *)&clientaddr, &clientlen);
-                getnameinfo((SA *)&clientaddr, clientlen, client_hostname, MAXLINE, client_hostname, MAXLINE, 0);
-                printf("Connected with (%s, %s) -> FD %d.\n", connfd, client_hostname, client_portname);
-                ev.events = EPOLLIN | EPOLLHUP;
+                getnameinfo((SA *)&clientaddr, clientlen, client_hostname, MAXLINE, client_portname, MAXLINE, 0);
+                printf("Connected with (%s, %s) -> FD %d.\n", client_hostname, client_portname, connfd);
+                ev.events = EPOLLIN | EPOLLRDHUP;
                 ev.data.fd = connfd;
                 epoll_ctl(epollfd, EPOLL_CTL_ADD, connfd, &ev);
             } else {
                 if (events[i].events & EPOLLIN) {
+                    printf("EPOLLIN");
                     read(fd1, buf, MAXLINE);
-                    dprintf(STDOUT_FILENO, "%s", buf);
+                    printf("%s", buf);
+                    fflush(0);
                     dprintf(fd1, "Receive %d bytes.\n", strlen(buf));
-                } else if (events[i].events & (EPOLLERR | EPOLLHUP)) {
+                } 
+                if (events[i].events & (EPOLLERR | EPOLLRDHUP)) {
+                    printf("EPOLLERR | EPOLLRDHUP");
                     epoll_ctl(epollfd, EPOLL_CTL_DEL, fd1, NULL);
                     close(fd1);
-                    dprintf(STDOUT_FILENO, "Disconnected with FD %d\n", fd1);
+                    printf("Disconnected with FD %d\n", fd1);
                 }
             }
         }
